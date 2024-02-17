22dd5c51067b24ffb9a1e68ae027a2bae9adff6c
Akshat Srivastava <107216804+Akshat-Sri7@users.noreply.github.com>
2024-02-18 01:15:38 +0530

    Create EtchAsketch.py

---
+++ Etch_A_Sketch/EtchAsketch.py
@@ -0,0 +1,31 @@
+import turtle
+
+arthur = turtle.Turtle()
+
+
+def move_fwd():
+    arthur.forward(10)
+
+def move_bwd():
+    arthur.backward(10)
+
+def counter_clock():
+    arthur.left(10)
+
+def clockwise():
+    arthur.right(10)
+
+def clear_screen():
+    arthur.reset()
+
+
+screen = turtle.Screen()
+screen.listen()
+
+screen.onkey(key="w", fun=move_fwd)
+screen.onkey(key="s", fun=move_bwd)
+screen.onkey(key="a", fun=counter_clock)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
